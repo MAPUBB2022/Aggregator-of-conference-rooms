1a014f2227204a171b67e0c46da022f5dd0b1115
andreeatam <114987008+andreeatam@users.noreply.github.com>
2022-12-29 14:59:54 +0200

    Add files via upload

---
+++ src/main/java/view/Main.java
@@ -0,0 +1,13 @@
+
+import view.InvalidDataException;
+import view.View;
+
+
+public class Main {
+    public static void main(String[] args) throws InvalidDataException {
+
+     View view = new View();
+     view.runProgram();
+
+    }
+}
